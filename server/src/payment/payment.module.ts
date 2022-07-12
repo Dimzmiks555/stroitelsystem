505e6d14eb85f44bdd42099594d6c9eef9b07e6b
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2022-07-12 17:12:04 +0300

    'payment_event'

--- server/src/payment/payment.module.ts
+++ server/src/payment/payment.module.ts
@@ -3,9 +3,13 @@ import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Payment } from './entities/payment.entity';
+import { EventModule } from 'src/event/event.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Payment])],
+  imports: [
+    SequelizeModule.forFeature([Payment]),
+    EventModule
+  ],
   controllers: [PaymentController],
   providers: [PaymentService]
 })
--- server/src/payment/payment.service.ts
+++ server/src/payment/payment.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Contragent } from 'src/contragents/entities/contragent.entity';
 import { Deal } from 'src/deals/entities/deal.entity';
+import { EventService } from 'src/event/event.service';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
 import { Payment } from './entities/payment.entity';
@@ -11,16 +12,36 @@ export class PaymentService {
 
   constructor(
     @InjectModel(Payment)
-    private paymentModel: typeof Payment
+    private paymentModel: typeof Payment,
+
+    private eventService: EventService
   ){}
 
 
-  create(createPaymentDto) {
+  async create(createPaymentDto) {
+
+
     console.log(createPaymentDto)
-    return this.paymentModel.create(createPaymentDto)
+
+    let payment = await this.paymentModel.create(createPaymentDto)
+
+    let eventData = {
+      type: 'CREATE',
+      entity_id: payment.id,
+      entity: 'payment',
+      // previousData: JSON.stringify(prevNote),
+      realData: JSON.stringify(createPaymentDto)
+    }
+
+    let event = this.eventService.create(eventData)
+
+    return payment
   }
 
   findAll(query) {
+
+
+    
     return this.paymentModel.findAndCountAll({
       where: query,
       include: [
@@ -39,7 +60,24 @@ export class PaymentService {
     });
   }
 
-  update(id, updatePaymentDto) {
+  async update(id, updatePaymentDto) {
+
+
+    let payment = await this.paymentModel.findOne({
+      where: {id}
+    })
+
+    let eventData = {
+      type: 'UPDATE',
+      entity_id: id,
+      entity: 'payment',
+      previousData: JSON.stringify(payment),
+      realData: JSON.stringify(updatePaymentDto)
+    }
+
+    let event = this.eventService.create(eventData)
+
+
     return this.paymentModel.update(updatePaymentDto, {
       where: {id}
     });
